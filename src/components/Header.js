1f140bffc7e5684d02e4bc780acb7ec7c064f06e
Krum Kuzmov <kkuzmov@gmail.com>
2021-07-18 17:50:07 +0300

    add logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import HeaderStyle from './Header.module.css'
-import logo from '../logo/satellite_logo.png'
+import React from "react";
+import HeaderStyle from "./Header.module.css";
+import logo from "../logo/satellite_logo.png";
 
 function Header() {
   return (
     <header className={HeaderStyle.header}>
-      <figure><img src={ logo } className={HeaderStyle.logo}></img></figure>
+      <figure className={HeaderStyle.figure}>
+        <img src={logo} className={HeaderStyle.logo}></img>
+      </figure>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
