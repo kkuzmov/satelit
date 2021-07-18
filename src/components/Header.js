081ee5fbe4c4c51ecf5e5c097d0692fe16334b7f
Krum Kuzmov <kkuzmov@gmail.com>
2021-07-19 00:04:25 +0300

    add menu nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HeaderStyle from "./Header.module.css";
-import logo from '../logo/Satellite-X-logo-white.png';
+import logo from "../logo/Satellite-X-logo-white.png";
+import { FiAlignJustify } from "react-icons/fi";
 
 function Header() {
   return (
     <header className={HeaderStyle.header}>
+      <FiAlignJustify className={HeaderStyle.icon}/>
       <figure className={HeaderStyle.figure}>
         <img src={logo} className={HeaderStyle.logo}></img>
       </figure>
