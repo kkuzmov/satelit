16d64cc2dc3b90040b843467265f319dc38918b2
Krum Kuzmov <kkuzmov@gmail.com>
2021-07-21 17:08:59 +0300

    add collapsible nav

--- src/components/DropdownNav.js
+++ src/components/DropdownNav.js
@@ -3,7 +3,7 @@ import DropdownNavStyles from "./DropdownNav.module.css";
 import { CgMenuGridO } from "react-icons/cg";
 
 function DropdownNav() {
-  const [navClass, setNavClass] = useState('')
+  const [navClass, setNavClass] = useState('navHidden')
 
   function toggleNav(){
     console.log(navClass)
@@ -14,7 +14,7 @@ function DropdownNav() {
   return (
     <nav className={DropdownNavStyles.nav}>
       <CgMenuGridO className={DropdownNavStyles.icon} onClick={toggleNav}/>
-      <ul className={DropdownNavStyles.navClass}>
+      <ul className={DropdownNavStyles[navClass]}>
         <li>Home</li>
         <li>Test</li>
         <li>Staff</li>
