0ab9a0e7e94e3d0fb92571020ed119d4b5ddb84a
Krum Kuzmov <kkuzmov@gmail.com>
2021-07-25 12:50:47 +0300

    move dropdown nav to header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import Header from "./components/Header";
-// import Footer from "./components/Footer";
 import BottomNav from "./components/BottomNav";
-import DropdownNav from "./components/DropdownNav";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <DropdownNav />
       <BottomNav />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import React from "react";
 import HeaderStyle from "./Header.module.css";
 import logo from "../logo/Satellite-X-logo-white.png";
+import DropdownNav from "./DropdownNav";
+
 
 function Header() {
   return (
+    <>
+    <DropdownNav />
     <header className={HeaderStyle.header}>
       <figure className={HeaderStyle.figure}>
         <img src={logo} className={HeaderStyle.logo}></img>
       </figure>
     </header>
+    </>
   );
 }
 
