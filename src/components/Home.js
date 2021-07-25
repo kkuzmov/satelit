80c30b9e41fd04faec8e54354f7a6b63c86b356d
Krum Kuzmov <kkuzmov@gmail.com>
2021-07-25 13:44:40 +0300

    fix headings styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,11 @@ import HomeStyles from "./Home.module.css";
 
 function Home() {
   return (
-    <div>
+    <div className={HomeStyles.homepageHeadings}> 
       <h1 className={HomeStyles.mainHeading}>Welcome to Satellite X social club</h1>
       <h2 className={HomeStyles.secondaryHeading}>Enjoy our photos and stories</h2>
       <h2 className={HomeStyles.secondaryHeading}>Create your own</h2>
+      {/* <h1 className={HomeStyles.testHeading}>TESTING SATELLITE X</h1> */}
     </div>
   );
 }
