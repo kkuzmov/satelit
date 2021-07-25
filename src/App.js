12562f832629e48d41b9270c593d7eeda2f36abb
Krum Kuzmov <kkuzmov@gmail.com>
2021-07-25 12:55:37 +0300

    add home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
 import BottomNav from "./components/BottomNav";
-
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <BottomNav />
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HomeStyles from "./Home.module.css";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Satellite X social club</h1>
+      <h2>Enjoy our photos and stories</h2>
+      <h2>Create your own</h2>
+    </div>
+  );
+}
+
+export default Home;
