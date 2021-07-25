02753c0ab4715b012dc527b23f85d74b4e521396
Krum Kuzmov <kkuzmov@gmail.com>
2021-07-25 13:25:17 +0300

    add headings styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ import HomeStyles from "./Home.module.css";
 function Home() {
   return (
     <div>
-      <h1>Welcome to Satellite X social club</h1>
-      <h2>Enjoy our photos and stories</h2>
-      <h2>Create your own</h2>
+      <h1 className={HomeStyles.mainHeading}>Welcome to Satellite X social club</h1>
+      <h2 className={HomeStyles.secondaryHeading}>Enjoy our photos and stories</h2>
+      <h2 className={HomeStyles.secondaryHeading}>Create your own</h2>
     </div>
   );
 }
