96dfdb4ebc465d93405f2d634e10f3350729058b
Krum Kuzmov <kkuzmov@gmail.com>
2021-07-26 18:42:30 +0300

    staff and story heading fix

--- src/components/Staff.js
+++ src/components/Staff.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from './Header';
 import BottomNav from './BottomNav';
+import StaffStyles from "./Staff.module.css";
+
 
 function Staff() {
   return (
     <>
     <Header />
+    <div className={StaffStyles.staffHeading}>
       <h1>This is staff...</h1>
+    </div>
     <BottomNav />
     </>
   )
--- src/components/Story.js
+++ src/components/Story.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from './Header'
 import BottomNav from './BottomNav'
+import StoryStyles from "./Story.module.css";
 
 function Story() {
   return (
     <>
     <Header />
+      <div className={StoryStyles.storyHeading}>
       <h1>This is story...</h1>
+      </div>
     <BottomNav />
     </>
   )
