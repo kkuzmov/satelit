5570e7d8a06deb899cb79cd6721422044d4f0a75
Krum Kuzmov <kkuzmov@gmail.com>
2021-07-26 12:45:21 +0300

    add rotating icon to header

--- src/components/DropdownNav.js
+++ src/components/DropdownNav.js
@@ -3,16 +3,17 @@ import DropdownNavStyles from "./DropdownNav.module.css";
 import { CgMenuGridO } from "react-icons/cg";
 
 function DropdownNav() {
-  const [navClass, setNavClass] = useState('navHidden')
+  const [navClass, setNavClass] = useState('navHidden');
+  const [iconClass, setIconClass] = useState('icon');
 
   function toggleNav(){
     navClass == 'navVisible' ? setNavClass('navHidden') : setNavClass('navVisible')
+    iconClass == 'icon' ? setIconClass('iconOnClick') : setIconClass('icon')
   }
-  
 
   return (
     <nav className={DropdownNavStyles.nav}>
-      <CgMenuGridO className={DropdownNavStyles.icon} onClick={toggleNav}/>
+      <CgMenuGridO className={DropdownNavStyles[iconClass]} onClick={toggleNav}/>
       <ul className={DropdownNavStyles[navClass]} id={DropdownNavStyles.navUl}>
         <li>Home</li>
         <li>Story</li>
