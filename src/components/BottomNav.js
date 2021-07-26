81e8caa6968ef24b08cf7c6a9292447eb83fdc53
Krum Kuzmov <kkuzmov@gmail.com>
2021-07-26 14:42:42 +0300

    add styles to bottom nav

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import BottomNavStyles from "./BottomNav.module.css";
-import LowerNavButton from "./LowerNavButton";
 
 function BottomNav() {
   return (
