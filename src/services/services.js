54247b218361016d4758fe7bd9e978d9501c1dc9
Krum Kuzmov <kkuzmov@gmail.com>
2021-07-28 11:25:23 +0300

    testing

--- src/services/services.js
+++ src/services/services.js
@@ -2,7 +2,3 @@ function toggleNav(navClass, iconClass, setNavClass, setIconClass){
   navClass == 'navVisible' ? setNavClass('navHidden') : setNavClass('navVisible')
   iconClass == 'iconDefault' ? setIconClass('iconRotated') : setIconClass('iconDefault')
 }
-
-export{
-  toggleNav,
-}
